b58d0fc357052bdeb2ce47e263a72efa90886490
rayaneamrouche <rayaneamrouche@outlook.com>
2026-07-30 20:22:24 +0200

    Upload index.html and images folder

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE_NAME = 'portfolio-offline-v1785433771458';
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(cacheNames => Promise.all(cacheNames.map(cacheName => { if (cacheName !== CACHE_NAME) return caches.delete(cacheName); }))).then(() => self.clients.claim())); });
